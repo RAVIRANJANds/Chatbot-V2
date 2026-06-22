b75f3c12cf367cc4fb39fea1bcb2b636224a72a0
RAVIRANJANds <lr4674092@gmail.com>
2026-06-22 12:23:50 +0530

    fix render secret file path

--- backend/app.py
+++ backend/app.py
@@ -16,11 +16,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SHEET_ID = "1UyvjshQcX7GR_IWao82dGTZ0IaGfktQMoFB0XmVIPIE"
 
-CREDS_FILE = os.path.join(
-    BASE_DIR,
-    "Data",
-    "credentials.json"
-)
+CREDS_FILE = "/etc/secrets/credentials.json"
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
